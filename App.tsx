a48e458e6ee8599e4ea9b8047cb6e226712abe9e
Hoooooou-Jun <jipkim2@gmail.com>
2021-10-26 16:26:53 +0900

    initial setting_1

--- App.tsx
+++ App.tsx
@@ -1,14 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = () => {
+    return (
+      <View style={styles.container}>
+        <Text>Hello React Native for TypeScript</Text>
+      </View>
+    )
 }
 
 const styles = StyleSheet.create({
@@ -19,3 +17,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
