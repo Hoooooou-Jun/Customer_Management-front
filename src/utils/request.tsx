2cc42ebba66eec7e040d03c0bf7cdb1eaa5f6745
Hoooooou-Jun <jipkim2@gmail.com>
2021-11-03 09:42:02 +0900

    Edit axios schema

--- src/utils/request.tsx
+++ src/utils/request.tsx
@@ -2,13 +2,13 @@ import { request } from './axios';
 
 const requestLogin = (ID: string, PW: string) => {
 	return request.post('/users/login', {
-		"username": ID,
+		"id": ID,
 		"password": PW,
 })};
 
 const requestSignUp = (ID: string, PW: string) => {
 	return request.post('/users/register', {
-		"username": ID,
+		"id": ID,
 		"password": PW,
 })};
 
